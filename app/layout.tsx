503fcfcf625919802838d7cdd7e1978ef1ec843b
92Ezio48 <vovanew77714@gmail.com>
2025-12-09 14:40:22 +0300

    v1.35

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 "use client";
 import Header from "@/components/Header/Header";
-import { AuthProvider } from "@/context/AuthContext"; // путь уточни свой!
+import { AuthProvider } from "@/context/AuthContext";
+import { Montserrat } from "next/font/google"; //💡
 import "./globals.css";
+// 💡 Подключение шрифта
+const montserrat = Montserrat({
+  subsets: ["latin", "cyrillic"],
+  weight: ["200", "400", "500", "600", "700"], // 💡Укажи нужные начертания
+  variable: "--font-montserrat", // 💡Можно использовать в CSS как var(--font-montserrat)
+  display: "swap",
+});
 export default function RootLayout({ children }) {
   return (
-    <html lang="ru">
+    <html lang="ru" className={montserrat.variable}>
       <body>
         <AuthProvider>
           <Header />
